9d3c9bf31dcb0b4ba1c0d6c57c3a0072ffcb1d86
JuicyAnt <chadwick0330@gmail.com>
2020-10-15 21:29:11 -0700

    added src folder n hello.java

---
+++ practice/Chadwick.java
@@ -0,0 +1,10 @@
+package practice;
+/*
+Chadwick Li
+*/
+
+class Chadwick{
+    public static void main(String[] args) {
+        System.out.println("Hello?"); 
+    }
+}
---
+++ practice/Hello.java
@@ -0,0 +1,10 @@
+package practice;
+/*
+Chadwick Li
+*/
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello?"); 
+        System.out.println("I think u guys continue on.");
+    }
+}
