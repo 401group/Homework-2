401c8505016ae727fd71a1852e8c88feb0d5f511
zMedeirosCSUEB <57689416+zMedeirosCSUEB@users.noreply.github.com>
2020-10-16 19:34:27 -0700

    Zack's first commit to the group project

--- practice/Hello.java
+++ practice/Hello.java
@@ -4,7 +4,7 @@ Chadwick Li
 */
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello?"); 
+        System.out.println("Zack's commit");
         System.out.println("I think u guys continue on.");
     }
 }
---
+++ practice/Zack.java
@@ -0,0 +1,10 @@
+package GroupProject;
+/*
+Zachary Medeiros
+ */
+
+public class Zack {
+    public static void main(String[] args) {
+        System.out.println("Team Skynet");
+    }
+}
